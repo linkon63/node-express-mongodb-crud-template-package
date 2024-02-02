f9040b8d10bce10e423a03880d22fd25c9ac08a9
linkon63 <m.alinkon10@gmail.com>
2024-02-02 15:48:40 +0600

    -------------

--- setup.js
+++ setup.js
@@ -1,4 +1,5 @@
-const { exec } = require("child_process");
+const { execSync } = require("child_process");
+
 const fs = require("fs");
 const path = require("path");
 
@@ -17,79 +18,85 @@ const jsFileContent = fs.readFileSync(filePath, "utf8");
 const serverJsContent = jsFileContent;
 const serverJsPath = path.join(parentDirectoryPath, "server.js");
 fs.writeFileSync(serverJsPath, serverJsContent);
+
 console.log("Working on installation");
-// install packages
-const packageNames = ["cors", "mongodb", "express", "body-parser"];
 
-// Specify the directory where you want to install the packages
 const installDirectory = path.join(__dirname, "../../"); // Adjust the number of '../' based on your directory structure
-exec(
-  `npm install`,
-  { cwd: installDirectory },
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error installing express: ${error.message}`);
-      return;
-    }
-    console.log(`Package 'express' installed successfully`);
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  }
-);
 
-exec(
-  `npm install cors`,
-  { cwd: installDirectory },
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error installing Cors: ${error.message}`);
-      return;
-    }
-    console.log(`Package 'Cors' installed successfully`);
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  }
-);
+// Example: Run a shell command synchronously
+try {
+  const result = execSync("npm install express mongodb cors body-parser", {
+    cwd: installDirectory,
+  });
+  console.log(result.toString()); // Output of the command
+} catch (error) {
+  console.error("Installation Error", error.stderr); // Error output
+}
 
-exec(
-  `npm install express`,
-  { cwd: installDirectory },
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error installing express: ${error.message}`);
-      return;
-    }
-    console.log(`Package 'express' installed successfully`);
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  }
-);
+// // install packages
+// const packageNames = ["cors", "mongodb", "express", "body-parser"];
 
-exec(
-  `npm install mongodb`,
-  { cwd: installDirectory },
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error installing mongodb: ${error.message}`);
-      return;
-    }
-    console.log(`Package 'mongodb' installed successfully`);
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  }
-);
+// // Specify the directory where you want to install the packages
+// const installDirectory = path.join(__dirname, "../../"); // Adjust the number of '../' based on your directory structure
 
-exec(
-  `npm install body-parser`,
-  { cwd: installDirectory },
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error installing body-parser: ${error.message}`);
-      return;
-    }
-    console.log(`Package 'body-parser' installed successfully`);
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  }
-);
+// const nodeC = async() => await exec(
+//   `npm install cors`,
+//   { cwd: installDirectory },
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error installing Cors: ${error.message}`);
+//       return;
+//     }
+//     console.log(`Package 'Cors' installed successfully`);
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   }
+// );
 
+// const nodeE = async() =>  await exec(
+//   `npm install express`,
+//   { cwd: installDirectory },
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error installing express: ${error.message}`);
+//       return;
+//     }
+//     console.log(`Package 'express' installed successfully`);
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   }
+// );
+
+// const nodeM = async() => await exec(
+//   `npm install mongodb`,
+//   { cwd: installDirectory },
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error installing mongodb: ${error.message}`);
+//       return;
+//     }
+//     console.log(`Package 'mongodb' installed successfully`);
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   }
+// );
+
+// const nodeBP = async() => await exec(
+//   `npm install body-parser`,
+//   { cwd: installDirectory },
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`Error installing body-parser: ${error.message}`);
+//       return;
+//     }
+//     console.log(`Package 'body-parser' installed successfully`);
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   }
+// );
+
+// // nodeI()
+// nodeE()
+// nodeM()
+// nodeC()
+// nodeBP()
